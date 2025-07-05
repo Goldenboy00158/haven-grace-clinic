70c23e2b1a032ae8e88f713df36c5fc301d90454
Goldenboy00158 <mwongelaisaiah4@gmail.com>
2025-07-05 08:48:45 +0300

    Updated ServicesManagement.tsx

--- src/components/ServicesManagement.tsx
+++ src/components/ServicesManagement.tsx
@@ -523,3 +523,4 @@ export default function ServicesManagement({ isReviewMode = false }: ServicesMan
     onChange={(e) => setNewService(prev => ({ ...prev, name: e.target.value }))}
     className="form-input"
   />
+</div>
